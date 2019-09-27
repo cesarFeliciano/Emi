98e484281f8229938868906d1c74ebea29162edb
cesarFeliciano <you@example.com>
2019-09-27 09:53:49 -0400

    Pizzeria.2

---
+++ Pizzeria.2/Pizzeria.2/Pizza.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria._2
+{
+    public class Pizza
+    {
+        public Pizza()
+        {
+            this.queso = new Queso();
+            this.tomate = new SalaTomate();
+        }
+        public Refresco refresco { get; set; }
+        public Queso queso { get; set; }
+        public SalaTomate tomate { get; set; }
+        
+    }
+    public class Queso
+    {
+
+    }
+    public class SalaTomate
+    {
+
+    }
+    public class Refresco
+    {
+        public string tipo { get; set; }
+    }
+}
